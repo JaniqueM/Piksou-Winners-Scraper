48d822dd8129a6077ca044e4167b53559fef0829
Janique <j.maduray@alustudent.com>
2026-08-12 21:42:56 +0400

    created two variables to contact the Winners website

--- scraper.py
+++ scraper.py
@@ -1,2 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
+import requests #allows Python to communicate with the website 
+from bs4 import BeautifulSoup #allows python to read and search the websites HTML 
+
+url = "https://www.winners.mu/"
+response = requests.get(url)
+print(response.status_code)
