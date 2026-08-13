b9e588f52e115821ccd053159d331da5a8a6b5f6
Janique <j.maduray@alustudent.com>
2026-08-13 21:14:20 +0400

    Tested that BeautifulSoup picks up specified elements.

--- scraper.py
+++ scraper.py
@@ -4,4 +4,9 @@ from bs4 import BeautifulSoup #allows python to read and search the websites HTM
 url = "https://www.winners.mu/"
 response = requests.get(url)
 print(response.status_code)
-print(response.text[:500])
+print(response.text[:500]) #from the website, we printed the first 500 characters as a test of connection 
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(response.text, "html.parser")
+
+print(soup.title)
