63b59d28632c321e888631fe5facb8e779e4d46a
Janique <j.maduray@alustudent.com>
2026-08-15 18:18:57 +0400

    Made single product scraper, printed out relevant details

--- scraper.py
+++ scraper.py
@@ -7,9 +7,26 @@ print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-product = soup.find("div", class_="product-item")
+product_text = soup.find(string=lambda text: text and "DANESITA" in text.upper())
 
-name = product.find("h2", class_="product-title")
-price = product.find("span", class_="price actual-price")
-print(name.text)
-print(price.text)
+print(product_text.parent.parent.parent)
+
+product_title = soup.find("h2", class_="product-title")
+
+product = product_title.parent
+
+name = product_title.get_text(strip=True)
+
+sku = product.find("div", class_="sku").get_text(strip=True)
+
+old_price = product.find("span", class_="old-price").get_text(strip=True)
+
+actual_price = product.find("span", class_="actual-price").get_text(strip=True)
+
+link = product_title.find("a")["href"]
+
+print("Name:", name)
+print("SKU:", sku)
+print("Old price:", old_price)
+print("Actual price:", actual_price)
+print("Link:", link)
