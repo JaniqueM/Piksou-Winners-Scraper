6554f196f1688ef8a3ed54897e42ec06212f53a0
Janique <j.maduray@alustudent.com>
2026-08-19 21:56:45 +0400

    Tried testing if pagination works

--- scraper.py
+++ scraper.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup #allows python to read and search the websites HTM
 
 url = "https://www.winners.mu/"
 response = requests.get(url)
-print(response.status_code)
+
+print("Status:", response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -12,25 +13,4 @@ products = soup.find_all("h2", class_="product-title")
 print("Number of products:", len(products))
 
 for product_title in products:
-
-    product = product_title.parent
-
-    name = product_title.get_text(strip=True)
-
-    sku_element = product.find("div", class_="sku")
-    sku = sku_element.get_text(strip=True) if sku_element else "N/A"
-
-    old_price_element = product.find("span", class_="old-price")
-    old_price = old_price_element.get_text(strip=True) if old_price_element else "N/A"
-
-    actual_price_element = product.find("span", class_="actual-price")
-    actual_price = actual_price_element.get_text(strip=True) if actual_price_element else "N/A"
-
-    link = product_title.find("a")["href"]
-
-    print("--------------------")
-    print("Name:", name)
-    print("SKU:", sku)
-    print("Old price:", old_price)
-    print("Actual price:", actual_price)
-    print("Link:", link)
+    print(product_title.get_text(strip=True))
