76d4f08e63d0b3357997027e06b8d1918a6d7ecd
Janique <j.maduray@alustudent.com>
2026-09-12 10:48:23 +0400

    Created a standardized product model for scraper engine data that can be used in any retailer website

---
+++ models/product.py
@@ -0,0 +1,16 @@
+class Product:
+    def __init__(
+        self,
+        product_id=None,
+        name=None,
+        sku=None,
+        price=None,
+        url=None,
+        category=None
+    ):
+        self.product_id = product_id
+        self.name = name
+        self.sku = sku
+        self.price = price
+        self.url = url
+        self.category = category
